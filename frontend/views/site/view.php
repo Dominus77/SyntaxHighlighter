9a633859185309155ca714a37bfeee73a495b7b9
Alexey Shevchenko <ivanovosity@gmail.com>
2017-10-02 11:34:35 +0300

    commit

--- frontend/views/site/view.php
+++ frontend/views/site/view.php
@@ -10,19 +10,7 @@ SyntaxHighlighterAsset::register($this);
 	<h2>Example code content</h2>
 
 	<pre class="brush:php;auto-links:false;html-script:true;toolbar:false" contenteditable="false">
-	<?php/**
-     * @param $id
-     * @return BlogPost the loaded model
-     * @throws NotFoundHttpException
-     */
-    protected function findModel($id)
-    {
-        if (($model = BlogPost::findOne(['id' => $id, 'status' => BlogPost::STATUS_PUBLISH])) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException(Module::t('module', 'The requested page does not exist.'));
-        }
-    }?>
+	echo "Hello World";
 	</pre>
 
 <p>&nbsp;</p>
